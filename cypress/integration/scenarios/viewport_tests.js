eebbd5873accdf603dcfa9bbadf9458c5ffc8d93
subrato29 <subratosarkar29@gmai.com>
2022-06-13 11:52:18 -0400

    added viewport tests

---
+++ cypress/integration/scenarios/viewport_tests.js
@@ -0,0 +1,34 @@
+'use strict'
+
+describe ('testing different view ports', () => {
+    
+    const url = 'https://www.flipkart.com/';
+
+    before (() => {
+        console.log ('running my tests');
+    })
+
+    beforeEach (() => {
+        cy.visit (url);
+    })
+
+    it ('open in macbook-13', () => {
+        cy.viewport ('macbook-13');
+        cy.screenshot ();
+    })
+
+    it ('open in macbook-15', () => {
+        cy.viewport ('macbook-15');
+        cy.screenshot ();
+    })
+
+    it ('open in iphone-x', () => {
+        cy.viewport ('iphone-x');
+        cy.screenshot ();
+    })
+
+    it ('open in 500 and 750', () => {
+        cy.viewport (550, 750);
+        cy.screenshot ();
+    })
+})
